3fe644d0fa4d84f8356e31f8f6caa87181f9c43e
FX-IT-Marina <121512261+FX-IT-Marina@users.noreply.github.com>
2023-07-11 17:07:51 +0200

    done task hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import css from './App.module.css';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './FilterForm/FilterForm';
+import { ContactList } from './ContactList/ContactList';
+import { v4 as uuidv4 } from 'uuid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  formSubmitHandler = data => {
+    const dataWithId = {
+      id: uuidv4(),
+      ...data,
+    };
+
+    this.state.contacts
+      .map(({ name }) => name.toLocaleLowerCase())
+      .includes(dataWithId.name.toLocaleLowerCase())
+      ? alert(`${dataWithId.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [dataWithId, ...prevState.contacts],
+        }));
+  };
+
+  filterHandler = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  onFilterChange = () =>
+    this.state.contacts.filter(({ name }) =>
+      name
+        .split(' ')
+        .join('')
+        .toLocaleLowerCase()
+        .includes(this.state.filter.toLocaleLowerCase())
+    );
+
+  deleteHandler = e => {
+    const filter = this.state.contacts.filter(
+      contact => contact.id !== e.currentTarget.parentNode.id
+    );
+    this.setState({ contacts: filter });
+  };
+
+  render() {
+    const {
+      formSubmitHandler,
+      filterHandler,
+      onFilterChange,
+      deleteHandler,
+      state,
+    } = this;
+
+    return (
+      <div className={css.formContainer}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={formSubmitHandler} />
+        <h2 className={css.title}>Contacts</h2>
+        <Filter onChange={filterHandler} value={state.filter} />
+        <ContactList contacts={onFilterChange()} onDelete={deleteHandler} />
+      </div>
+    );
+  }
+}
